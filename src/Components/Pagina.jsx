bcc1ca9a528968b4783367e37819eaa7e2873d52
Kenneth <Kennethmer28@outlook.com>
2022-10-31 11:14:59 -0500

    Sexto Commit: Correccion de boostrap y solucion de errores

--- src/Components/Pagina.jsx
+++ src/Components/Pagina.jsx
@@ -5,7 +5,7 @@ import {useState} from 'react';
 
 
 function Pagina() {
-    const [datos, setDatos] = useState([]);
+    const [datos, setDatos] = React.useState([]);
     //const [paginacion] = useState(1);
     
     const CargarInfo = async() =>{
@@ -20,9 +20,7 @@ function Pagina() {
         }
     }
 
-    if(datos.length === 1){
-        <div>Cargando... Click nuevamente</div>
-    }
+    
 
     const Siguiente = async() =>{  
         try{
@@ -49,27 +47,28 @@ function Pagina() {
     
 
   return (
-    <>
-        
+    <> 
         <h1> Datos Personas Api </h1>
-        <div className="container" >
+        <div className='container' >
         {datos.map((Info)=>(
-            <div className="box">
-            <div className="image">
+            <div className='box'>
+            <div className='image'>
                <img src={Info.avatar} alt=""/>
               </div>
-              <div className="name_job">{Info.first_name} {Info.last_name}</div>
+              <div className='name_job'> {Info.first_name} {Info.last_name}</div>
               <div className="rating">
                 <i className="fas fa-star">Correo: {Info.email}</i>
               </div>
               </div> 
         ))}
        </div>
-       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
-        <div class="d-grid gap-2 col-4 mx-auto">
-        <button onClick={CargarInfo} class="btn btn-primary" type="button">Obtener datos</button>
-        <button onClick={Siguiente}  class="btn btn-primary" type="button">Siguiente</button>
-        <button onClick={Anterior}  class="btn btn-primary" type="button">Anterior</button>
+       <div>
+       
+        <div >
+        <button className="Botom" onClick={CargarInfo} >Obtener datos</button>
+        <button className="Botom" onClick={Siguiente}   >Siguiente</button>
+        <button className="Botom" onClick={Anterior}   >Anterior</button>
+        </div>
         </div>
     </>
   )
